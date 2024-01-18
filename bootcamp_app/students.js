cf64b071a5af58500a088d70c07d22f5e022de92
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-01-18 12:03:10 -0500

    edit queries to be parameterized and protect against SQL injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,15 +8,17 @@ const pool = new Pool({
 });
 
 const cohort = process.argv[2];
-const limit = process.argv[3];
-
-pool.query(`
+const limit = process.argv[3] || 5;
+const values = [`%${cohort}%`, limit];
+const queryString = `
 SELECT students.id as id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${cohort}%'
-LIMIT ${limit || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(row => {
       const statement = `${row.name} has an id of ${row.id} and was in the ${row.cohort} cohort.`;
